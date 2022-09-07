ec0218140296191552ac5179c020800dd424d77e
alexNovik <alexnovik86@gmail.com>
2022-09-07 23:17:54 +0300

    Third commi

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const animItems = document.querySelectorAll("._anim-items");
+
+if (animItems.length > 0) {
+  window.addEventListener("scroll", animOnScroll);
+
+  function animOnScroll(params) {
+    for (let i = 0; i < animItems.length; i++) {
+      const animItem = animItems[i];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if (
+        window.pageYOffset > animItemOffset - animItemPoint &&
+        window.pageYOffset < animItemOffset + animItemHeight
+      ) {
+        animItem.classList.add("_active");
+      } else {
+        if (!animItem.classList.contains("_anim-no-hide")) {
+          animItem.classList.remove("_active");
+        }
+      }
+    }
+  }
+
+  // Функция получения смещения DOM-элемента относительно документа
+  function offset(el) {
+    const rect = el.getBoundingClientRect();
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + screenLeft };
+  }
+
+  setTimeout(() => {
+    animOnScroll();
+  }, 300);
+}
